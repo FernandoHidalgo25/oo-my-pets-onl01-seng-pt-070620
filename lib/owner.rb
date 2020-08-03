9fa06e137e13e07a34ccee8492288e9c32b02c99
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-03 21:39:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,67 +1,93 @@
-class Owner
-  attr_reader :species
-  attr_accessor :name, :pets, :dog, :cat
+require 'pry'
 
-  @@all = []
-  @@pets = {:dogs => [], :cats => []}
+class Owner
+  @@all= []
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  # Instance Methods #
 
   def initialize(species)
     @species = species
-    @name = name
     @@all << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  # Pets #
+
+  def buy_fish(name_of_fish)
+    @pets[:fishes] << Fish.new(name_of_fish)
+  end
+
+  def buy_dog(name_of_dog)
+    @pets[:dogs] << Dog.new(name_of_dog)
+  end
+
+  def buy_cat(name_of_cat)
+    @pets[:cats] << Cat.new(name_of_cat)
+  end
+
+  def walk_dogs
+    @pets.collect do |species, instances|
+      if species == :dogs
+        instances.each do |dog|
+          dog.mood = "happy"
+        end
+      end
+    end
+  end
+
+  def play_with_cats
+    @pets.collect do |species, instances|
+      if species == :cats
+        instances.each do |cat|
+          cat.mood = "happy"
+        end
+      end
+    end
+  end
+
+  def feed_fish
+    @pets.collect do |species, instances|
+      if species == :fishes
+        instances.each do |fish|
+          fish.mood = "happy"
+        end
+      end
+    end
+  end
+
+  def sell_pets
+    @pets.collect do |species, instances|
+      instances.each do |pet|
+        pet.mood = "nervous"
+      end
+      instances.clear
+    end
+  end
+
+  def list_pets
+    num_dogs = @pets[:dogs].size
+    num_cats = @pets[:cats].size
+    num_fish = @pets[:fishes].size
+    return "I have #{num_fish} fish, #{num_dogs} dog(s), and #{num_cats} cat(s)."
+  end
+
+  # Class Methods #
 
   def self.all
     @@all
-  end
-
-  def self.count
-    @@all.length
   end
 
   def self.reset_all
     @@all.clear
   end
 
-  def say_species
-    "I am a #{@species}."
+  def self.count
+    @@all.size
   end
-
-  def pets
-    @@pets
-  end
-
-  def buy_cat(cat)
-    @@pets[:cats] << Cat.new(cat)
-  end
-
-  def buy_dog(dog)
-    @@pets[:dogs] << Dog.new(dog)
-  end
-
-  def walk_dogs
-    @@pets[:dogs].each do |dog|
-      dog.mood = 'happy'
-    end
-  end
-
-  def play_with_cats
-    @@pets[:cats].each do |cat|
-      cat.mood = 'happy'
-    end
-  end
-
-  def list_pets
-    "I have #{pets[:fishes].length} dog(s), and #{pets[:cats].length} cat(s)."
-  end
-
-  def sell_pets
-    pets.each do |pet, arr|
-      arr.map do |pet|
-        pet.mood = 'nervous'
-      end
-      arr.clear
-    end
-  end
-
 end
